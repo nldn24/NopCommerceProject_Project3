70d1a97cdc0f42315f91f421cb69bc598ec41efe
BRC1234210 <askbarrcan41@gmail.com>
2025-10-31 17:22:00 +0300

    baris cansiz finished

--- src/test/java/US_503_PositiveAndNegativeLoginCombinations/US_503_PositiveAndNegativeLoginCombinations.java
+++ src/test/java/US_503_PositiveAndNegativeLoginCombinations/US_503_PositiveAndNegativeLoginCombinations.java
@@ -20,28 +20,24 @@ public class US_503_PositiveAndNegativeLoginCombinations extends BaseGUITest {
     @Test(priority = 1, description = "Open the browser and navigate to the Website. ")
     @Description("Navigate to Website.")
     @Severity(SeverityLevel.CRITICAL)
-    public void navigateToWebsite(){
+    public void navigateToWebsite() {
         homePage.verifyHomePage();
     }
 
     @Test(priority = 2, description = "The customer clicks the login button.")
     @Description("Click login button")
     @Severity(SeverityLevel.CRITICAL)
-    public void clickLoginBtn(){
+    public void clickLoginBtn() {
         homePage.clickLoginButton();
-        BaseDriver.threadWait(3);
+
     }
 
     @Test(priority = 3, description = "The customer enters an incorrect email address three times in the email field.",
-    dataProvider = "invalidLoginCredentials",dataProviderClass = TestDataProvider.class)
+            dataProvider = "invalidLoginCredentials", dataProviderClass = TestDataProvider.class)
     @Description("The customer enters an incorrect email address three times in the email field.")
     @Severity(SeverityLevel.CRITICAL)
-    public void invalidLogin(String email, String password){
-
-        BaseDriver.threadWait(2);
-        loginPage.fillLoginMask(email,password);
-        BaseDriver.threadWait(3);
-
+    public void invalidLogin(String email, String password) {
+        loginPage.fillLoginMask(email, password);
         if (!email.equals(ConfigReader.getProperty("email")) || !password.equals(ConfigReader.getProperty("password"))) {
             LOGGER.warn("❌ Invalid login -> Email: " + email + " | Password: " + password);
         } else {
